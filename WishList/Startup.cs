732a269869f044bd2b7b3e1f23dae30d22452f6f
Tobias Christian <tobias.christian@mailbox.org>
2021-01-29 23:53:40 +0100

    Configure exception handling

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,6 +23,14 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
